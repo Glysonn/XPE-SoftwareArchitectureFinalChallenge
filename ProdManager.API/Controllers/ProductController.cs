64379e19f8ea23f5250eb7d9e6102ac8c054e577
Glysonn <kauaglyson@gmail.com>
2025-02-06 23:08:46 -0300

    fix: change all Products route endpoints name to remove redundancy

--- ProdManager.API/Controllers/ProductController.cs
+++ ProdManager.API/Controllers/ProductController.cs
@@ -8,14 +8,14 @@ namespace ProdManager.API.Controllers
     [Route("[controller]")]
     public class ProductController(IProductService productService) : ControllerBase
     {
-        [HttpGet("GetAllProducts")]
+        [HttpGet("GetAll")]
         public IActionResult GetAllProducts() 
         {
             var products = productService.FindAllProducts();
             return Ok(products); 
         }
         
-        [HttpGet("GetProductById/{id:guid}")]
+        [HttpGet("GetById/{id:guid}")]
         public IActionResult GetProductById(Guid id)
         {
             var productDto = productService.FindProductById(id);
@@ -27,35 +27,35 @@ namespace ProdManager.API.Controllers
             return Ok(productDto);
         }
 
-        [HttpGet("GetProductsByName")]
+        [HttpGet("GetByName")]
         public IActionResult GetProductsByName([FromQuery] string name)
         {
             var products = productService.FindProductsByName(name);
             return Ok(products);
         }
         
-        [HttpGet("CountProducts")]
+        [HttpGet("Count")]
         public IActionResult CountProducts()
         {
             var count = productService.CountProducts();
             return Ok(count);
         }
 
-        [HttpPost("CreateProduct")]
+        [HttpPost("Create")]
         public IActionResult CreateProduct([FromBody] CreateProductDto dto)
         {
             productService.CreateProduct(dto);
             return Ok("Product created successfully.");
         }
 
-        [HttpPut("UpdateProduct")]
+        [HttpPut("Update")]
         public IActionResult UpdateProduct([FromBody] UpdateProductDto dto)
         {
             productService.UpdateProduct(dto);
             return Ok("Product updated successfully.");
         }
 
-        [HttpDelete("DeleteProduct/{id:guid}")]
+        [HttpDelete("Delete/{id:guid}")]
         public IActionResult DeleteProduct(Guid id)
         {
             productService.DeleteProduct(id);
